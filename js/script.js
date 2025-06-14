665b86969acfd1eabb961e8783c2b159d95f6653
LayanZaid <layanabuzaid906@gmail.com>
2025-06-14 16:03:52 +0300

    9

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+const from = document.getElementById('add-form');
+const addInput = document.querySelector('#add-input')
+const todos = JSON.parse(localStorage.getItem("todos")) || []
+function addTodo(e) {
+  e.preventDefault();
