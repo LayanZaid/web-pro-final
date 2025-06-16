6745dd4ed9ea6a215c7abd3614a1e6a8a5084098
LayanZaid <layanabuzaid906@gmail.com>
2025-06-16 16:28:08 +0300

    21

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function displayTodos() {
       <tr>
       <td>${index + 1}</td>
         
-        <td>${item.isUpdating ? <input type="text" data-index="${index}" value="${item.todo}"/> : item.todo}</td>
+        <td>${item.isUpdating ? `<input type="text" data-index="${index}" value="${item.todo}"/>` : item.todo}</td>
 
         <td><input onclick="toggleCompleted(${index})" type="checkbox" ${item.completed ? 'checked' : ""}  /></td>
 
@@ -36,10 +36,124 @@ function displayTodos() {
           ${item.isUpdating ? ` <button class="btn btn-success"
             onclick="saveTodo(${index})"><i class="fa-solid fa-file-pen"></i></button>`
              :
-        <button class="btn btn-primary" onclick="setUpdating(${index})"><i class="fa-solid fa-pen"></i></button>}
+        `<button class="btn btn-primary" onclick="setUpdating(${index})"><i class="fa-solid fa-pen"></i></button>`}
 
         <button onclick="deleteTodo(${index})" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
          </td>
       </tr>
     `
   })
+
+  document.getElementById('todos').innerHTML = result;
+}
+
+function toggleCompleted(index) {
+  todos[index].completed = !todos[index].completed;
+  console.log(todos);
+  localStorage.setItem('todos', JSON.stringify(todos))
+  displayTodos()
+}
+
+function deleteTodo(index) {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      todos.splice(index, 1)
+      localStorage.setItem('todos', JSON.stringify(todos))
+      displayTodos()
+
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success"
+      });
+    }
+  });
+}
+
+function setUpdating(index) {
+  todos[index].isUpdating = true;
+  localStorage.setItem('todos', JSON.stringify(todos))
+  displayTodos()
+
+}
+
+function saveTodo(index) {
+  const input = document.querySelector(`input[data-index="${index}"]`)
+  todos[index].todo = input.value.trim()
+  todos[index].isUpdating = false;
+  localStorage.setItem('todos', JSON.stringify(todos))
+  displayTodos()
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 3000
+  });
+}
+displayTodos()
+from.addEventListener('submit', addTodo)
+
+
+
+
+
+
+
+
+
+
+
+function filterTodos(status) {
+  let filtered = todos;
+
+  if (status === "done") {
+    filtered = todos.filter(todo => todo.completed);
+  } else if (status === "todo") {
+    filtered = todos.filter(todo => !todo.completed);
+  }
+
+  displayFilteredTodos(filtered);
+}
+
+function displayFilteredTodos(filteredList) {
+  let result = '';
+  filteredList.forEach((item, index) => {
+    result += `
+      <tr>
+        <td>${index + 1}</td>
+        <td>${item.isUpdating ?`<input type="text" data-index="${index}" value="${item.todo}"/> `: item.todo}</td>
+        <td><input onclick="toggleCompleted(${index})" type="checkbox" ${item.completed ? 'checked' : ""} /></td>
+        <td>
+          ${item.isUpdating ? `<button class="btn btn-success" onclick="saveTodo(${index})"><i class="fa-solid fa-file-pen"></i></button>`
+        : `<button class="btn btn-primary" onclick="setUpdating(${index})"><i class="fa-solid fa-pen"></i></button>`}
+          <button onclick="deleteTodo(${index})" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+        </td>
+      </tr>
+    `;
+  });
+
+  document.getElementById('todos').innerHTML = result;
+}
+
+function deleteDoneTasks() {
+  const newList = todos.filter(todo => !todo.completed);
+  todos.length = 0;
+  todos.push(...newList);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  displayTodos();
+}
+
+function deleteAllTasks() {
+  todos.length = 0;
+  localStorage.setItem("todos", JSON.stringify(todos));
+  displayTodos();
+}
