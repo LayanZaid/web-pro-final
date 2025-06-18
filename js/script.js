abf6a94ea05cda83067c52af8621dadcb3a13e4b
RawyaNaji <r.naji@student.aaup.edu>
2025-06-18 23:59:08 +0300

    16

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ function saveTodo(index) {
   todos[index].isUpdating = false;
   localStorage.setItem('todos', JSON.stringify(todos))
   displayTodos()
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 3000
+  });
+}
+displayTodos()
+from.addEventListener('submit', addTodo)
