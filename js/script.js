7ff7ee115a1a04fae37e11a7da0f58b2afccae82
RawyaNaji <r.naji@student.aaup.edu>
2025-06-18 23:55:54 +0300

    15

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 function saveTodo(index) {
   const input = document.querySelector(input[data-index="${index}"])
   todos[index].todo = input.value.trim()
+  todos[index].isUpdating = false;
+  localStorage.setItem('todos', JSON.stringify(todos))
+  displayTodos()
